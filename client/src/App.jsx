647e962c5b34ebd92fc434eb30a075c5471cff4b
gr.priyk <priyankgaur2005@gmail.com>
2026-01-09 22:04:57 +0530

    Made navbar and home page plus user auth using Clerk

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,21 @@
-import { useState } from 'react'
-import React from 'react'
+import { useState } from "react";
+import React from "react";
+import Navbar from "./components/Navbar.jsx";
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home';
 
 const App = () => {
+  const isOwnerPath = useLocation().pathname.includes("owner");
   return (
     <div>
+      {!isOwnerPath && <Navbar />}
+      <div className="min-h-[70vh]">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
