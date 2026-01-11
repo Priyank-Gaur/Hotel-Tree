1449e846620349d72ffe03244e5c704ec269c230
gr.priyk <priyankgaur2005@gmail.com>
2026-01-11 16:28:47 +0530

    ok

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -14,12 +14,11 @@ const clerkWebhooks = async (req, res) => {
         }
         await whook.verify(JSON.stringify(req.body), headers);
 
-        const evt = whook.verify(req.body, headers);
-        const { data, type } = evt;
+        const { data, type } = req.body;
         const userData = {
             _id: data.id,
             email : data.email_addresses[0].email_address,
-            username : data.first_name + " " + data.last_name,
+            name : data.first_name + " " + data.last_name,
             image : data.image_url,
         }
 
